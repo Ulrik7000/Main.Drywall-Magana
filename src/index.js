98bed575a4b1d220186d76a20395223c27acdf08
Ulrik7000 <82905858+Ulrik7000@users.noreply.github.com>
2021-10-15 17:21:41 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -9,17 +9,17 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 
-import { store, persistor } from './redux/store';
+// import { store, persistor } from './redux/store';
 
 import * as serviceWorker from './serviceWorker';
 import App from './App';
 
 ReactDOM.render(
-<Provider store={store}>
-  <PersistGate persistor={persistor}>
+// <Provider store={store}>
+//   <PersistGate persistor={persistor}>
   <App />
-  </PersistGate>
-  </Provider>,
+//   </PersistGate>
+//   </Provider>,
   document.getElementById('root')
 );
 
