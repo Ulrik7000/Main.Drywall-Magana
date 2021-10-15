efae2fdfe090a518ffa4f1c402f6558a8dbdecf5
Ulrik7000 <82905858+Ulrik7000@users.noreply.github.com>
2021-10-15 17:30:17 -0400

    Update header.component.jsx

--- src/views/Ecommerce/components/header/header.component.jsx
+++ src/views/Ecommerce/components/header/header.component.jsx
@@ -6,11 +6,11 @@ import { createStructuredSelector } from 'reselect';
 // import { auth } from '../../../../firebase/firebase.utils';
 import CartIcon from '../cart-icon/cart-icon.component';
 import CartDropdown from '../cart-dropdown/cart-dropdown.component';
-import { selectCartHidden } from '../../../../redux/cart/cart.selectors';
-import { selectCurrentUser } from '../../../../redux/user/user.selectors';
+// import { selectCartHidden } from '../../../../redux/cart/cart.selectors';
+// import { selectCurrentUser } from '../../../../redux/user/user.selectors';
 
 
-import { ReactComponent as Logo } from '../../../../../src/assets/crown.svg'
+// import { ReactComponent as Logo } from '../../../../../src/assets/crown.svg'
 
 import './header.styles.css';
 
