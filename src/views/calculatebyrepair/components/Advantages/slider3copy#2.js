7da934929616e97f838a5eb9dca598633e63c2f0
Ulrik7000 <82905858+Ulrik7000@users.noreply.github.com>
2022-02-08 05:59:27 -0500

    Update slider3copy#2.js

--- src/views/calculatebyrepair/components/Advantages/slider3copy#2.js
+++ src/views/calculatebyrepair/components/Advantages/slider3copy#2.js
@@ -264,7 +264,7 @@ style={{color: '#8a8a8a'}}>
               <Input
               className={classes.input}
               value={
-              (sqft * (height*0.125))*3}
+              (sqft * (height*0.15))*3}
 
 
               margin="dense"
